59faac8fcb0659963461277ae5aa7abdcb8c914b
Tomek <kemot15@tlen.pl>
2021-05-29 18:11:36 +0200

    dev and mock config add

--- ClientApp/src/app/core/post.service.ts
+++ ClientApp/src/app/core/post.service.ts
@@ -2,13 +2,13 @@ import { HttpClient } from "@angular/common/http";
 import { Inject, Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Post } from "../models/post";
-
+import {environment} from "src/environments/environment";
 @Injectable({
   providedIn: "root",
 })
 export class PostService {
-//   private url = "http://localhost:3001/posts";
-  private url = this.baseUrl + 'api/posts' //@Inject('BASE_URL') private baseUrl: string
+  private url = environment.api.path + '/posts';
+  // private url = this.baseUrl + 'api/posts' //@Inject('BASE_URL') private baseUrl: string
 
   constructor(private httpClient: HttpClient, @Inject('BASE_URL') private baseUrl: string) {}
 
